0769023bc402f3e066d0522696e2a1c1db9da92a
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-17 16:26:53 +0200

    Create pyVariables03.py

---
+++ source_code/pyVariables03.py
@@ -0,0 +1,11 @@
+# pyVariables03.py
+# more variables...
+
+var01 = 1
+print("var01 is ", type(var01), " and its value is: ", var01)
+
+var01 = float(var01)
+print("var01 is ", type(var01), " and its value is: ", var01)
+
+var01 = str(5)
+print("var01 is ", type(var01), " and its value is: ", var01)
