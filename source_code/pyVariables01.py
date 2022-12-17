dda93fe603e70ccebab6868623a133f4471dc986
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-17 13:59:47 +0200

    Create pyVariables01.py

---
+++ source_code/pyVariables01.py
@@ -0,0 +1,13 @@
+# pyVariables01.py
+# variables...
+
+var01 = 1
+print(var01)
+
+msg1 = "Hi from NKUA Di!"
+msg2 = 'Hi from AUEB Di!'
+var01 = 'Hi from the Milky Way!!!'
+
+print(msg1)
+print(msg2)
+print(var01)
