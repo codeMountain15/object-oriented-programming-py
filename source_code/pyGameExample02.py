9a90e85904548fe2cf314110a1b43b47c4a4a33e
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-18 20:20:20 +0200

    Update pyGameExample02.py

--- source_code/pyGameExample02.py
+++ source_code/pyGameExample02.py
@@ -1,5 +1,14 @@
 # pyGameExample02.py
 
+"""
+ Implelenting pygame library
+ for a simple python program
+ that opens a new window,
+ shows a text message
+ and changes its color
+ with the keyboard arrows
+"""
+
 import pygame
  
 # Define the color options
@@ -7,6 +16,7 @@ BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+BLACK = (0, 0, 0)
 
 # Define the dimensions
 width = 800
@@ -28,33 +38,36 @@ pygame.display.set_caption("Warming up for Object Oriented Programming with Pyth
 # that is already in pygame
 # 40 is the font size
 fontObject = pygame.font.Font('freesansbold.ttf', 40)
- 
-# create a blue message on the surface of the font object
-# the fonte color is BLUE in this example
-# antialias is True, so the characters will have smooth edges
-message = fontObject.render('Fon\'t forget to submit the WnV project!', True, BLUE)
- 
-# create a rectangular object for the
-# text surface object
-textRect = message.get_rect()
- 
-# set the center of the rectangular object.
-textRect.center = (width / 2, height / 2)
- 
-# fill the surface object
-# with white color
-surfaceObject.fill(WHITE)
- 
-# copying the text surface object
-# to the display surface object
-# at the center coordinate.
-surfaceObject.blit(message, textRect)
 
 # for the loop
 exit = False
 
+nextColor = BLUE
+
 # main program loop
 while not exit:
+
+    # create a blue message on the surface of the font object
+    # the font color is stored in the nextColor variable
+    # antialias is True, so the characters will have smooth edges
+    message = fontObject.render('Fon\'t forget to submit the WnV project!', True, nextColor)
+ 
+    # create a rectangular object for the
+    # text surface object
+    textRectObject = message.get_rect()
+ 
+    # set the center of the rectangular object
+    # in the center of the canvas
+    textRectObject.center = (width / 2, height / 2)
+ 
+    # fill the surface object
+    # with white color
+    surfaceObject.fill(WHITE)
+ 
+    # copy the message (=text surface object)
+    # to the surface object
+    # at the coordinates mentrined above
+    surfaceObject.blit(message, textRectObject)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -63,13 +76,13 @@ while not exit:
         # keyboard arrows
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            screen.fill(RED)
+            nextColor = RED
         if keys[pygame.K_RIGHT]:
-            screen.fill(BLUE)
+            nextColor = BLUE
         if keys[pygame.K_UP]:
-            screen.fill(GREEN)
+           nextColor = GREEN
         if keys[pygame.K_DOWN]:
-            screen.fill(WHITE)
+            nextColor = BLACK
 
         # Draws the surface object on the screen
         pygame.display.update()
