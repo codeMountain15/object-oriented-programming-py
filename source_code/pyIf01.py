7db74af0ac05c973a5cbf07dda7fba93ccc14e64
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-18 10:19:03 +0200

    Create pyIf01.py

---
+++ source_code/pyIf01.py
@@ -0,0 +1,12 @@
+# pyIf01.py
+# if .. elif ... else
+
+print("Type your Institution")
+institution = input()
+
+if institution == "NKUA":
+  print("National & Kapodistrian University of Athens, the best")
+elif institution == "AUEB":
+  print("In Athens University of Economics and Business we trust")
+else:
+  print("Di forever!")
