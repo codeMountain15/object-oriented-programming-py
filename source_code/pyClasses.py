2af7330e0ec8462ebf508439c5f71ec135cae364
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-18 21:25:34 +0200

    Create pyClasses.py

---
+++ source_code/pyClasses.py
@@ -0,0 +1,9 @@
+# pyClasses.py
+# a simple class example
+
+class Student:
+    name = "Babis"
+    department = "Informatics"
+
+obj01 = Student()
+print(obj01.name + " from the " + obj01.department + " department")
