a83d686752ef62385913736e9fd801b53aa81241
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-19 21:37:32 +0200

    Create pyLists.py

---
+++ source_code/pyLists.py
@@ -0,0 +1,11 @@
+# pyLists.py
+# 
+
+# list
+cholisterini_party_list = ["patates tiganites", "souvlakia", "pizza", "kontosouvli"]
+for i in range(len(cholisterini_party_list)):
+    print(cholisterini_party_list[i])
+
+# list with several values
+list_m = ["I am ", 1002, " years old ", " and it is ", True]
+print(list_m)
