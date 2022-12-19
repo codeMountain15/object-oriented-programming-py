090ab078b8fc927e38e26566deda5998abec29f0
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-19 22:14:03 +0200

    Create pyDictionaries.py

---
+++ source_code/pyDictionaries.py
@@ -0,0 +1,14 @@
+# pyDictionaries.py
+# 
+
+# dictionary
+academic_milestones = {
+  "bachelor": "AUEB: Informatics",
+  "master": "HOU: Adult Education",
+  "phd": "Ionian University: HCI",
+  "graduation_year": ["498 B.C.", "1022 A.D.", "1821 A.D."],
+  "scholar": True
+}
+
+for i, j in academic_milestones.items():
+    print(i, " -> ", j)
