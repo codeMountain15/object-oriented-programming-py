679f8ee1379a301448e4c00665606db54e8351cd
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-21 20:06:01 +0200

    Create pyClasses05a.py

---
+++ source_code/pyClasses05a.py
@@ -0,0 +1,20 @@
+# pyClasses05a.py
+#
+
+class Dog():
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+
+class strayDog(Dog):
+  pass
+
+obj01 = strayDog("Mourgos", "unknown")
+
+# add a new attribute to obj01
+#obj01.weight = 10
+setattr(obj01, "weight", 15)
+
+print(obj01.name)
+print(obj01.category)
+print(obj01.weight)
